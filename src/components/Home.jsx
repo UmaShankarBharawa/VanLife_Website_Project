9f5b3e787e3ddbb62858500f16f3f453a316bf51
Uma Shankar Bharawa <ubharawa@gmail.com>
2024-09-13 00:20:00 +0530

    Update component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
         <h1 className="home--heading">You got the travel plans, we got the travel vans.</h1>
-        <p className="home--decription">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <p className="home--description">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
         <Link className="findVanBtn">Find your van</Link>
     </div>
   )
